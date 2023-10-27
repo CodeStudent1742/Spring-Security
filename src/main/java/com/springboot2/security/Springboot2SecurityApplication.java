0ee902078e9644481960e814c47c72cd87cc814d
Albert Piechnik <madrian@drawdistance.dev>
2023-10-27 19:54:50 +0200

    WebSecurityCongig

---
+++ src/main/java/com/springboot2/security/Springboot2SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot2.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2SecurityApplication.class, args);
+	}
+
+}
