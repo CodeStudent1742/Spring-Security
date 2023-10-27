c3dbb48474f55165e13e9c23835da3f770fa9477
Albert Piechnik <madrian@drawdistance.dev>
2023-10-27 21:40:26 +0200

    Configure

---
+++ src/main/java/com/springboot2/security/TestApi.java
@@ -0,0 +1,24 @@
+package com.springboot2.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApi {
+
+    @GetMapping("/forAll")
+    public String forAll() {
+        return "forAll";
+    }
+
+    @GetMapping("/forUser")
+    public String forUser() {
+        return "forUser";
+    }
+
+    @GetMapping("/forAdmin")
+    public String forAdmin() {
+        return "forAdmin";
+    }
+
+}
--- src/main/java/com/springboot2/security/WebSecurityConfig.java
+++ src/main/java/com/springboot2/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot2.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -31,5 +32,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth.inMemoryAuthentication().withUser(userAdmin);
         auth.inMemoryAuthentication().withUser(userUser);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/forAdmin").hasRole(("ADMIN"))
+                .antMatchers("/forUser").hasRole("USER")
+                .antMatchers("/forAll").hasAnyRole();
+
     }
 }
