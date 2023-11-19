11e1bcff9d17c81fec2b12564584b0b9107062d4
Albert Piechnik <madrian@drawdistance.dev>
2023-11-19 15:15:48 +0100

    User Entity Creation

--- SecurityBefore2.7/src/main/java/com/springboot2/security/AppUserRepo.java
+++ SecurityBefore2.7/src/main/java/com/springboot2/security/AppUserRepo.java
@@ -1,2 +1,8 @@
-package com.springboot2.security;public class AppUserRepo {
+package com.springboot2.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepo extends JpaRepository<AppUser,Long> {
 }
--- SecurityBefore2.7/src/main/java/com/springboot2/security/Start.java
+++ SecurityBefore2.7/src/main/java/com/springboot2/security/Start.java
@@ -1,2 +1,23 @@
-package com.springboot2.security;public class Start {
+package com.springboot2.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Start {
+
+    private PasswordEncoder passwordEncoder;
+    private AppUserRepo appUserRepo;
+
+    @Autowired
+    public Start(PasswordEncoder passwordEncoder, AppUserRepo appUserRepo) {
+        this.passwordEncoder = passwordEncoder;
+        this.appUserRepo = appUserRepo;
+
+        AppUser appUser = new AppUser();
+        appUser.setUsername("Przemek");
+        appUser.setPassword(passwordEncoder.encode("Przemek123"));
+        appUserRepo.save(appUser);
+    }
 }
