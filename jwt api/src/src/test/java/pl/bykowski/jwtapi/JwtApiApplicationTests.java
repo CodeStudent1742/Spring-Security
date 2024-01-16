99c5b74428cbe5a1280852eceda42688dad69f6f
Albert P <albert1742@gmail.com>
2024-01-16 20:48:57 +0100

    jwt.api

---
+++ jwt api/src/src/test/java/pl/bykowski/jwtapi/JwtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bykowski.jwtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
